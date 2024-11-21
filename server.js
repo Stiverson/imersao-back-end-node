840c68aada0902df710a906aed437df24c5356d5
tiagrobill <ti@agrobill.com.br>
2024-11-21 17:15:05 -0300

    wip aula 3 35 minutos

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+
+
 
 const posts = [
     {
@@ -26,18 +31,28 @@ app.listen(3000,() => {
     console.log(" Servidor escutando ...");
 });
 
-app.get("/posts", (req, res) => {
+
+ async function getTodososPosts(){
+ 
+  const db = conexao.db("imersao-stiverson")
+  const colecao = db.collection(posts)
+  return colecao.find().toArray()
+
+}
+
+app.get("/posts", async (req, res) => {
+    const posts = await getTodososPosts()
     res.status(200).json(posts);
 });
 
-function buscarPostPorID(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
+// function buscarPostPorID(id){
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id)
+//     })
+// }
 
-app.get("/posts/:id", (req, res) => {
+// app.get("/posts/:id", (req, res) => {
     
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
-});
+//     const index = buscarPostPorID(req.params.id)
+//     res.status(200).json(posts[index]);
+// });
