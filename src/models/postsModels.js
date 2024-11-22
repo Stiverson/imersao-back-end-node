c61d652e45a1690de9c6309fb92f38b33157d54c
tiagrobill <ti@agrobill.com.br>
2024-11-22 16:35:34 -0300

    wip finalização aula 5 e preparação deploy

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -1,10 +1,12 @@
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
 // Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para buscar todos os posts do banco de dados
 export async function getTodosPosts() {
-    // Seleciona o banco de dados "imersao-instabytes"
+    // Seleciona o banco de dados "imersao-stiverson"
     const db = conexao.db("imersao-stiverson");
     // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
@@ -15,5 +17,12 @@ export async function getTodosPosts() {
 export async function criarPost(novoPost) {
     const db = conexao.db("imersao-stiverson");
     const colecao = db.collection("posts");
-    return colecao.insertOne(novoPost)
+    return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-stiverson");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
 }
